9f6602b03b7f3763da04cba262b436eef68fe2cc
Will <williamcarter808@gmail.com>
2026-04-19 17:36:18 +1000

    Added SRC assignment update to hourly tasks

--- hourly.py
+++ hourly.py
@@ -3,6 +3,7 @@ import assignRuns
 import database.verifiers
 import requests
 import database.Interface as dbi
+import updateSrcAssignments
 
 
 db = dbi.Interface("veri.db")
@@ -11,3 +12,4 @@ stateUpdate.cullStaleRecords(db) # Get rid of any assignments/users correspondin
 assignRuns.updateAssignments(db) # Assign any straggler runs (runs that obsolete a run already assigned)
 for verifier in database.verifiers.getActiveVerifiers(db): 
     res = requests.post(f"http://localhost:6767/hourly/{verifier.discordId}") # Update verifiers' personal queues
+updateSrcAssignments.main()
